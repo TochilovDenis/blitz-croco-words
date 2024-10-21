e7e90a810b83f5cfc02dbb56edac10dcf9014fce
Stdliber <iablokovvs@mail.ru>
2024-10-21 19:50:45 +0300

    Add zipfile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from zipfile import ZipFile
 
 
 def cur():
@@ -7,5 +8,15 @@ def cur():
     return current_directory
 
 
+PATH_FILE_ZIP = "F:/AcademyTop/blitz-croco-words/src/croco-blitz-source.zip"
+
+
+def show_zip():
+    with ZipFile(PATH_FILE_ZIP, mode="r") as archive:
+        for file_name_zip in archive.namelist():
+            print(file_name_zip)
+
+
 if __name__ == "__main__":
-    print(cur())
+    # print(cur())
+    show_zip()
