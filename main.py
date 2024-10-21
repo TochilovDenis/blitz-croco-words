5b8eaec3bc03196a0a90e44899ff581bfd2e1456
Stdliber <iablokovvs@mail.ru>
2024-10-21 19:31:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import os
 
-print(os.path.dirname(os.path.abspath(__file__)))
+
+def cur():
+    current_file = os.path.realpath(__file__)           # текущий файл
+    current_directory = os.path.dirname(current_file)   # текущая папка
+    return current_directory
+
+
+if __name__ == "__main__":
+    print(cur())
