c292d2a76a25cb9e92d7fa1db13e64212c927e33
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-26 14:07:28 +0300

    Задача 2024.10.21.06 решена
    С помощью ZipFile.namelist() выведите список всех файлов внутри архива на экран.
    
    Добавлена функция для списка файлов в zip-архиве
    
    - Реализована функцию list_archive_files() для получения списка всех файлов в zip-архиве
    - Импортировать необходимые модули: os, zipfile и pathlib
    - Определить константы для путей к исходному коду и имени архива
    - Обновлена функция main() для вывода списка файлов в архиве
    - Добавлена аннотации типов для улучшения читаемости и кода
    - Добавлена документацию к новой функции

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os  # Импорт модуля os для работы с операционной системой
+from zipfile import ZipFile  # Импорт класса ZipFile из модуля zipfile для работы с zip-файлами
+from pathlib import Path # Импорт класса Path из модуля pathlib для удобной работы с путями
+
+# Константы
+SRC = 'src'  # Папка с исходным кодом
+FILENAME_ZIP = 'croco-blitz-source.zip'  # Имя файла архива
 
 
 def current_dir() -> str:
@@ -15,9 +21,27 @@ def current_dir() -> str:
     return current_folder
 
 
+def list_archive_files() -> list[str]:
+    """
+    Список всех файлов в zip-архиве.
+    Returns:
+    list[str]: Список имен файлов в архиве.
+    """
+    # Открываем zip-файл в текущей директории, в папке src, с именем FILENAME_ZIP
+    with ZipFile(Path(current_dir()) / SRC / FILENAME_ZIP, 'r') as zFile:
+        # Возвращаем список всех файлов в архиве
+         return zFile.namelist()
+
+
 def main() -> None:
     # Выводим путь к текущему каталогу
     print(f"Путь: {current_dir()}")
+
+    # Выводим список файлов в архиве
+    print("Список файлов в архиве: ")
+    file_list: list[str] = list_archive_files()
+    for file in file_list:
+        print(file)
 
 
 if __name__ == "__main__":
