9cf0e21cbea5e0b3596fe92f44f086b9af02137d
TochilovDenis <denis-tochilov@yandex.ru>
2024-10-26 15:53:21 +0300

    Задача 2024.10.23.06 решена
    Добавлена функцию извлечения файлов из zip-архива
    
    - Реализована функцию extract_function_name() для извлечения файлов из zip-архива
    - Добавлена константа EXTRACTION_DIR для хранения пути к директории извлечения
    - Обновлена функция main() для демонстрации использования новой функции извлечения
    - Убедиться, что путь к архиву корректно формируется с помощью Path и текущего каталога

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import os  # Импорт модуля os для работы с операционной системой
 from zipfile import ZipFile  # Импорт класса ZipFile из модуля zipfile для работы с zip-файлами
 from pathlib import Path # Импорт класса Path из модуля pathlib для удобной работы с путями
+from pptx import Presentation # Импорт класса Presentation из модуля pptx для работы с презентациями
 
 # Константы
 SRC = 'src'  # Папка с исходным кодом
 FILENAME_ZIP = 'croco-blitz-source.zip'  # Имя файла архива
+EXTRACTION_DIR: str = "PPTX"  # Директория для извлечения файлов презентаций
+
+# Словарь с номерами и соответствующими им именами файлов презентаций
+PPTX: dict[int, str] = {
+    3: "Osennyaya_igra_3.pptx",
+    1: "Zimnyaya_igra_1.pptx",
+    12: "Osennyaya_igra_12.pptx",
+    11: "Osennyaya_igra_11.pptx",
+    10: "Osennyaya_igra_10.pptx",
+    9: "Osennyaya_igra_9.pptx",
+    6: "Osennyaya_igra_6.pptx",
+    5: "Osennyaya_igra_5.pptx",
+    4: "Osennyaya_igra_4.pptx"
+}
 
 
 def current_dir() -> str:
@@ -33,6 +48,48 @@ def list_archive_files() -> None:
             print(file)
 
 
+def extract_pptx_from_zip(src: str, filename_zip: str, extraction_dir: str):
+    """
+    Извлекает файлы презентаций из zip-архива.
+    Args:
+    src (str): Папка с исходным кодом
+    filename_zip (str): Имя файла архива
+    extraction_dir (str): Директория для извлечения файлов
+    """
+    with ZipFile(Path(current_dir()) / src / filename_zip, 'r') as zFile:
+        zFile.extractall(path=extraction_dir)
+
+    print(f"Файлы из {filename_zip} распакованы в {extraction_dir}")
+
+
+def open_pptx_with_pptx_library(extract_dir: str, pptx_file: dict[int, str], number: int,):
+    """
+    Открывает и выводит содержимое выбранной презентации.
+
+    Args:
+    extract_dir (str): Директория с извлеченными файлами презентаций
+    pptx_file (dict[int, str]): Словарь с номерами и именами файлов презентаций
+    number (int): Номер выбранной презентации
+    """
+    try:
+        pptx: str = pptx_file[number]    # Получаем имя файла презентации по указанному номеру
+        path: str = os.path.join(extract_dir, pptx)  # Создаем полный путь к файлу презентации
+        print(path)    # Выводим путь к файлу презентации
+        prs = Presentation(path)  # Открываем презентацию
+        print("Презентация открыта.")
+        print(f"Количество слайдов: {len(prs.slides)}") # Выводим количество слайдов в презентации
+        # Проходим по всем слайдам в презентации
+        for slide in prs.slides:
+            # Для каждого слайда проходим по всем шаблонам (shapes)
+            for shape in slide.shapes:
+                # Проверяем, имеет ли шаблон текст
+                if hasattr(shape, 'text'):
+                    # Если у шаблона есть текст, выводим его
+                    print(shape.text)
+    except Exception as e:
+        print(f"Ошибка при открытии презентации: {e}")
+
+
 def main() -> None:
     # Выводим путь к текущему каталогу
     print(f"Путь: {current_dir()}")
@@ -40,6 +97,11 @@ def main() -> None:
     # Выводим список файлов в архиве
     print("Список файлов в архиве: ")
     list_archive_files()
+
+    # Извлекаем файл презентации
+    extract_pptx_from_zip(SRC, FILENAME_ZIP, EXTRACTION_DIR)
+    # Открываем и выводим содержимое выбранной презентации
+    open_pptx_with_pptx_library(EXTRACTION_DIR, PPTX, 12)
 
 
 if __name__ == "__main__":
