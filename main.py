99afe5b6c6b3a7a954344f17bbebe5f2f3ef530f
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-01 19:00:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -182,15 +182,15 @@ def main() -> None:
     print("Список файлов в архиве: ")
     list_archive_files(SRC, FILENAME_ZIP)
     # Извлекаем файл презентации
-    # extract_pptx_from_zip(SRC, FILENAME_ZIP, EXTRACTION_DIR)
-    # # Открываем и выводим содержимое выбранной презентации
-    # open_file_pptx: int = randint(1, 9)
-    # open_pptx_with_pptx_library(EXTRACTION_DIR, PPTX, open_file_pptx)
-    #
-    # # Извлекаем слова из презентации
-    # extracted_words: list[str] = extract_words_from_pptx(EXTRACTION_DIR, PPTX, open_file_pptx)
-    # # Записываем слова в файл
-    # write_txt(FILENAME_TXT, extracted_words)
+    extract_pptx_from_zip(SRC, FILENAME_ZIP, EXTRACTION_DIR)
+    # Открываем и выводим содержимое выбранной презентации
+    open_file_pptx: int = randint(1, 9)
+    open_pptx_with_pptx_library(EXTRACTION_DIR, PPTX, open_file_pptx)
+
+    # Извлекаем слова из презентации
+    extracted_words: list[str] = extract_words_from_pptx(EXTRACTION_DIR, PPTX, open_file_pptx)
+    # Записываем слова в файл
+    write_txt(FILENAME_TXT, extracted_words)
 
     zip_file_path: Path = Path(SRC) / FILENAME_ZIP
     words: list[str] = extract_pptx_text(zip_file_path)
