c1818753d40a7aa4e0b85ba53e6dee32988a67dc
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-04 12:40:45 +0300

    Добавлена функция speller_word() для проверки орфографии отдельного слова с использованием библиотеки pyaspeller.
    Реализована обработка различных случаев проверки, включая исправленный вариант, варианты исправления и безопасность от опечаток.
    
    Обновлен код основной функции speller_text() для улучшения читаемости и структуры.

--- speller.py
+++ speller.py
@@ -1,13 +1,50 @@
-from pyaspeller import YandexSpeller
+from pyaspeller import YandexSpeller, Word
 
 
-def main():
-    spell: YandexSpeller = YandexSpeller()
-    letters: str = "в суббботу утромъ"
-    fixed: str = spell.spelled(letters)
+def speller_text():
+    """
+    Функция использует библиотеку pyaspeller, которая интегрируется с API Яндекса для проверки орфографии.
+    Проверка орфографии целых предложений или отдельных слов
+    Предложение вариантов исправления опечаток
+    :return: None
+    """
+    spell: YandexSpeller = YandexSpeller()  # создаем класс YandexSpeller
+    letters: str = "в суббботу утромъ"      # определяем строка для проверки
+    fixed: str = spell.spelled(letters)     # используем метод spelled() класса YandexSpeller для исправления
     print(fixed)
 
 
-if __name__ == '__main__':
-    main()
+def speller_word():
+    """
+    Проверяет орфографию отдельного слова.
+    Выводит информацию о правильности слова, вариантах исправления и безопасности от опечаток
+    :return: None
+    """
+    word_to_check: str = "Programm"
+    spell_checker: Word = Word(word_to_check)  # создаем объект типа Word для проверки
 
+
+    # Проверка условия: исправленный вариант слова.
+    if spell_checker.correct:
+        print(f"Исправленное слово: {spell_checker.correct}")
+    else:
+        print(f"Слово '{word_to_check}' не содержит опечаток")
+
+
+    # Проверка условия: список вариантов исправления, если они есть.
+    if spell_checker.variants:
+        print(f"Варианты исправления для '{word_to_check}': {spell_checker.variants}")
+    else:
+        print(f"Для слова '{word_to_check}' нет вариантов исправления")
+
+
+    # Проверка условия: указывает на безопасность слова от опечаток.
+    if spell_checker.spellsafe:
+        print(f"Слово '{word_to_check}' считается безопасным от опечаток")
+    else:
+        print(f"Слово '{word_to_check}' не является безопасным от опечаток")
+
+
+if __name__ == '__main__':
+    speller_text()
+    speller_word()
