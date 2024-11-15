7d5b691b8465406cac8aae02ae3bdff64dacb9ef
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-15 20:43:07 +0300

    Задача 2024.11.15.17

--- db.py
+++ db.py
@@ -2,17 +2,27 @@ import sqlite3  # для работы с SQLite базами данных
 
 
 def main() -> None:
+    # Устанавливаем соединение с базой данных SQLite
     con = sqlite3.connect('words.db')
+
+    # Создаем объект курсора для выполнения SQL-запросов
     cur = con.cursor()
 
-    cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
+    # Создаем таблицу 'movie' с колонками title, year и score
+    # Используем IF NOT EXISTS, чтобы избежать ошибок при повторном создании таблицы
+    cur.execute("CREATE TABLE IF NOT EXISTS movie(title TEXT, year INTEGER, score REAL)")
 
+    # Создаем список данных для вставки
     data = [
         ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
         ("Monty Python's The Meaning of Life", 1983, 7.5),
         ("Monty Python's Life of Brian", 1979, 8.0),
     ]
+
+    # Используем executemany для вставки всех данных за один запрос
     cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
+
+    # Сохраняем изменения в базе данных
     con.commit()
 
 
