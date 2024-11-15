3ea93a800760ca907596f1d33d78e1bc27728104
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-15 20:27:19 +0300

    Задача 2024.11.15.14

--- db.py
+++ db.py
@@ -6,7 +6,14 @@ def main() -> None:
     cur = con.cursor()
 
     cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
-    
+
+    cur.execute("""
+            INSERT INTO movie VALUES
+                ('Monty Python and the Holy Grail', 1975, 8.2),
+                ('And Now for Something Completely Different', 1971, 7.5)
+        """)
+    con.commit()
+
 
 if __name__ == '__main__':
     main()
