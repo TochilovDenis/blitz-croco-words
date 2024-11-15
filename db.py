34c23147b4f598cd99a610e9d20be5dfcb184826
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-15 19:22:45 +0300

    Задача 2024.11.15.08
    Обновление вывод результатов запроса к системному каталогу SQLite
    
    - Выполнен запрос SELECT name FROM sqlite_master для получения списка таблиц

--- db.py
+++ db.py
@@ -6,6 +6,10 @@ def main() -> None:
     cur = con.cursor()
     con.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
 
+    res = con.execute("SELECT name FROM sqlite_master")
+    print(type(res))  # <class 'sqlite3.Cursor'>
+    print(res)  # <sqlite3.Cursor object at 0x0000019942026F40>
+
 
 if __name__ == '__main__':
     main()
