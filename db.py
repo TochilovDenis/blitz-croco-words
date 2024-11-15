744720986c11834dc387a0b58029048c04099837
TochilovDenis <denis-tochilov@yandex.ru>
2024-11-15 20:41:50 +0300

    Задача 2024.11.15.15

--- db.py
+++ db.py
@@ -7,11 +7,12 @@ def main() -> None:
 
     cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
 
-    cur.execute("""
-            INSERT INTO movie VALUES
-                ('Monty Python and the Holy Grail', 1975, 8.2),
-                ('And Now for Something Completely Different', 1971, 7.5)
-        """)
+    data = [
+        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
+        ("Monty Python's The Meaning of Life", 1983, 7.5),
+        ("Monty Python's Life of Brian", 1979, 8.0),
+    ]
+    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
     con.commit()
 
 
